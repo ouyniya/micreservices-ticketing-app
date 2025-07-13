cb2d21f905bdbb682b0e1e039cfbb2a28a37deb6
niya-s <niya.somkerd@gmail.com>
2025-07-13 17:52:29 +0700

    feat: create auth service, auth k8s set up, and adding skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!!!!')
-})
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!@@@@");
+});
 
 const PORT = 3500;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
