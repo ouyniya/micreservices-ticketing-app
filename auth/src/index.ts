c2b181f91eb6f6d847228e30a047529e585aa88c
niya-s <niya.somkerd@gmail.com>
2025-07-14 13:53:43 +0700

    feat: revised auth service and connet to google cloud

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,43 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!@@@@");
-});
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+const start = async () => {
+
+  if (!process.env.JWT_KEY) {
+    console.log("Please define JWT key")
+    return
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongo db");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 const PORT = 3500;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/api/users/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || typeof email !== "string") {
+    res.status(400).send("Provide a valid email");
+  }
+
+  if (!password || typeof password !== "string") {
+    res.status(400).send("Provide a valid password");
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    console.log("Email in use...");
+    return res.send({});
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  // generate JWT
+  const userJWT = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  console.log(userJWT);
+
+  // Store it on session object
+  req.session = {
+    jwt: userJWT,
+  };
+
+  console.log("Creating a user...");
+
+  res.status(201).send(user);
+});
+
+export { router as signupRouter };
