4d726357f244ee63e019760acc1476e44164e926
niya-s <niya.somkerd@gmail.com>
2025-07-30 09:00:22 +0700

    feat: create index ticket test and route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import cookieSession from "cookie-session";
 import { currentUser } from "@nysticket/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -19,5 +20,6 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 export { app };
